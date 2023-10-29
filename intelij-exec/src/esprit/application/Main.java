90ddd0778c5730e787f8640eceb15161d4c20450
Manaa Ahmed <ahmed.manaa@esprit.tn>
2023-10-29 18:10:37 +0100

    intellijexec

---
+++ intelij-exec/src/esprit/application/Main.java
@@ -0,0 +1,28 @@
+package esprit.application;
+import esprit.tools.JenaEngine;
+import org.apache.jena.rdf.model.*;
+public class Main {
+    /**
+     * @param args
+     * the command line arguments
+     */
+    public static void main(String[] args) {
+        String NS = "";
+// lire le model a partir d'une ontologie
+        Model model = JenaEngine.readModel("data/WebSemantique.rdf");
+        if (model != null) {
+//lire le Namespace de l’ontologie
+            NS = model.getNsPrefixURI("");
+// apply our rules on the owlInferencedModel
+            Model inferedModel =
+
+                    JenaEngine.readInferencedModelFromRuleFile(model, "data/rules.txt");
+// query on the model after inference
+            System.out.println(JenaEngine.executeQueryFile(inferedModel,
+
+                    "data/query.txt"));
+
+        } else {
+            System.out.println("Error when reading model from ontology");
+        }
+    }}
