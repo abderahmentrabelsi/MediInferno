82e618fd65fea1b2d53d4801a080b2265dccec03
karimtrabelsi <karim.trabelsi1@esprit.tn>
2023-10-30 18:29:41 +0100

    getequipmentbyname with sparql

--- antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/EquipmentController.java
+++ antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/EquipmentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -36,4 +37,25 @@ public class EquipmentController {
 
         return ResponseEntity.ok(rdfService.queryRDFJson(query));
     }
+    @GetMapping("/getByName/{name}")
+    public ResponseEntity<JsonNode> getByName(@PathVariable("name") String name) {
+
+        //language=SPARQL
+        String query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX sante: <http://www.semanticweb.org/msi/ontologies/2023/9/sante_ont#>\n" +
+                "\n" +
+                "SELECT ?nom ?docteur \n" +
+                "WHERE {\n" +
+                "  ?equipement rdf:type/rdfs:subClassOf* sante:Equipement_Medical ;\n" +
+                "                            sante:aPourNom ?nom ;\n" +
+                "                            sante:utilisePar ?doc .\n" +
+                "\n" +
+                "  ?doc sante:aPourNom ?docteur .\n" +
+                "  FILTER (str(?nom) = \"" + name + "\")\n" +
+                "}\n";
+
+        return ResponseEntity.ok(rdfService.queryRDFJson(query));
+    }
+
 }
