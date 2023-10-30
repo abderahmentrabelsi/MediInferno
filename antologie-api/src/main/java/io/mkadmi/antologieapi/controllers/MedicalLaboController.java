fdac71a7dcddd79d5ca4c26a901e8d69bdf15826
Manaa Ahmed <ahmed.manaa@esprit.tn>
2023-10-30 18:32:03 +0100

    SearchAddToMedicalLabo&Products

--- antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/MedicalLaboController.java
+++ antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/MedicalLaboController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -37,4 +38,27 @@ public class MedicalLaboController {
 
         return ResponseEntity.ok(rdfService.queryRDFJson(query));
     }
+
+    @GetMapping("/getByName/{name}")
+    public ResponseEntity<JsonNode> getMedicalLaboByName(@PathVariable("name") String name) {
+
+        //language=SPARQL
+        String query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "PREFIX sante: <http://www.semanticweb.org/msi/ontologies/2023/9/sante_ont#>\n" +
+                "\n" +
+                "SELECT ?aPourNom ?aTechniciens ?aEquipement ?aProduitNom\n" +
+                "WHERE {\n" +
+                "  ?MedicalLab rdf:type/rdfs:subClassOf* sante:Medical_Labo ;\n" +
+                "              sante:aPourNom ?aPourNom ;\n" +
+                "              sante:aTechniciens ?aTechniciens ;\n" +
+                "              sante:aEquipement ?aEquipement ;\n" +
+                "              sante:aProduit ?aProduit .\n" +
+                "              ?aProduit sante:aPourNom ?aProduitNom .\n" +
+                "  FILTER (str(?aPourNom) = \"" + name + "\")\n" +
+                "}";
+
+        return ResponseEntity.ok(rdfService.queryRDFJson(query));
+    }
+
 }
--- frontend/app/products/productsComponent.js
+++ frontend/app/products/productsComponent.js
@@ -3,11 +3,20 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Card, CardHeader, CardBody, Image, Button , Input } from "@nextui-org/react";
 import Swal from "sweetalert2";
+import { useQuery } from "@tanstack/react-query";
 
 const ProductComponent = () => {
     const [products, setProducts] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
 
+    const { data: searchProduct, error } = useQuery({
+        queryKey: ['products', searchTerm],
+        queryFn: async () => {
+          return await fetch(
+            `http://localhost:8020/product/getByName/${searchTerm}`
+          ).then((res) => res.json());
+        }
+      });
 
     useEffect(() => {
         axios.get("http://localhost:8020/product/list")
@@ -55,12 +64,17 @@ const ProductComponent = () => {
         });
     };
 
-    const filteredProducts = products.filter(
-        (product) =>
-            product.aPourNom.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            product.pharmacieName.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
+    const searchProductByName = (name) => {
+        axios.get(`http://localhost:8020/product/getByName/${name}`)
+            .then(response => {
+                setProducts(response.data); // Update products with the search results
+            })
+            .catch(error => {
+                console.error("Error fetching products by name:", error);
+            });
+    };
+    
+   if (searchProduct && searchProduct.length > 0) {
     return (
         <div>
         <div className="flex justify-center items-center">
@@ -72,8 +86,50 @@ const ProductComponent = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
+      <br></br>
         <div style={{ display: "flex", flexWrap: "wrap" }}>
-            {filteredProducts.map(product => (
+            {searchProduct.map(product => (
+                <Card className="py-4" key={product.id} style={cardStyle}>
+                    <CardHeader className="pb-0 pt-2 px-4" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <div style={productInfoStyle}>
+                            <p className="text-tiny uppercase font-bold">{product.aPourNom}</p><br></br>
+                            <small className="text-default-500">{product.aAvecDosage}</small><br></br>
+                            <small className="text-default-500">{product.aDesInstructions}</small><br></br>
+                        </div>
+                    </CardHeader>
+                    <CardBody className="overflow-visible py-2" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <Image
+                            alt="Product Image"
+                            className="object-cover rounded-xl"
+                            src={"/images/medicaments.png"}
+                            width={270}
+                        />
+                        <div className="flex flex-wrap gap-4 items-center">
+                            <Button color="default" onClick={() => showDetails(product)}>
+                                Show Details
+                            </Button>
+                        </div>
+                    </CardBody>
+                </Card>
+            ))}
+        </div>
+        </div>
+    );
+   }
+    return (
+        <div>
+        <div className="flex justify-center items-center">
+        <Input
+          type="text"
+          label="Search by Name or Phamarcy Name"
+          className="max-w-xs"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <br></br>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+            {products.map(product => (
                 <Card className="py-4" key={product.id} style={cardStyle}>
                     <CardHeader className="pb-0 pt-2 px-4" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <div style={productInfoStyle}>
