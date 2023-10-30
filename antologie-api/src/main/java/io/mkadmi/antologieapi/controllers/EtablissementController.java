6b3032ecf0e136a450e2b8cd5b6bb071fad6de78
aboud <abderrahmen.trabelsi@esprit.tn>
2023-10-30 20:00:33 +0100

    update

--- antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/EtablissementController.java
+++ antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/EtablissementController.java
@@ -60,6 +60,19 @@ public class EtablissementController {
         var etablissementList = rdfService.queryToList(query, EtablissementDeSanteDTO.class);
         return ResponseEntity.ok(etablissementList);
     }
+    @GetMapping("/stats")
+    public ResponseEntity<JsonNode> getEtablissementStats() {
+        String query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX sante: <http://www.semanticweb.org/msi/ontologies/2023/9/sante_ont#>\n" +
+                "SELECT (COUNT(?etablissement_de_sante) as ?totalEtablissements) (MAX(?capacite) as ?maxCapacity) (MIN(?capacite) as ?minCapacity)\n" +
+                "WHERE {\n" +
+                "  ?etablissement_de_sante rdf:type sante:Etablissement_de_sante.\n" +
+                "  ?etablissement_de_sante sante:aUneCapacite ?capacite.\n" +
+                "}";
+
+        JsonNode stats = rdfService.queryRDFJson(query);
+        return ResponseEntity.ok(stats);
+    }
 
 
 }
