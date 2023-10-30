5427a710e040fca2220bf768bc12e1b97f68c1c2
Abdlmomen <abdelmoomen.medhioub@esprit.tn>
2023-10-30 21:07:42 +0100

    front vaccin

---
+++ frontend/app/vaccins/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import VaccinsComponent from './vaccinsComponent';
+
+
+const ProductsPage = () => {
+  const titleStyle = {
+    textAlign: 'center', 
+    fontSize: '36px',   
+    marginBottom: '24px', 
+  };
+
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', fontSize: '2rem' }}>Diseases List</h1>
+      <br></br>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        < VaccinsComponent/>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsPage;
