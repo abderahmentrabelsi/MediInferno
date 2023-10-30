bf36f5857e75324dd098e81e4229c27ff5ba4e5d
khalil <khalil.turki@esprit.tn>
2023-10-30 22:11:01 +0100

    Vehicules + salles added

--- antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/TraitementController.java
+++ antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/TraitementController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/traitements")  // Update the endpoint to match "traitements"
@@ -34,4 +36,24 @@ public class TraitementController {
 
         return ResponseEntity.ok(rdfService.queryRDFJson(query));
     }
+
+//    @GetMapping("/listByProfessor/{professorName}")
+//    public ResponseEntity<JsonNode> listTraitementsByProfessor(@PathVariable String professorName) {
+//        // Updated SPARQL query for Traitement filtered by professor name
+//        String query =  "PREFIX sante: <http://www.semanticweb.org/msi/ontologies/2023/9/sante_ont#>\n" +
+//                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+//                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+//                "SELECT ?aPourNomTrait ?aPourMateriels ?aPourDureeTrait ?aPourMedicaments ?aPourUnProfDeSante\n" +
+//                "WHERE {\n" +
+//                "  ?traitement rdf:type/rdfs:subClassOf* sante:Traitement.\n" +
+//                "  ?traitement sante:aPourNomTrait ?aPourNomTrait.\n" +
+//                "  ?traitement sante:aPourMateriels ?aPourMateriels.\n" +
+//                "  ?traitement sante:aPourDureeTrait ?aPourDureeTrait.\n" +
+//                "  ?traitement sante:aPourMedicaments ?aPourMedicaments.\n" +
+//                "  ?traitement sante:aPourUnProfDeSante ?aPourUnProfDeSante.\n" +
+//                "  FILTER(STR(?aPourUnProfDeSante) = \"" + professorName + "\")\n" +
+//                "}";
+//
+//        return ResponseEntity.ok(rdfService.queryRDFJson(query));
+//    }
 }
