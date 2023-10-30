df016f5daff26a63dbf6be2ffe33dd7e37e3a7a5
karimtrabelsi <karim.trabelsi1@esprit.tn>
2023-10-30 11:37:28 +0000

    prof controller

--- antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/ProfController.java
+++ antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/ProfController.java
@@ -20,19 +20,18 @@ public class ProfController {
         //language=SPARQL
         String query =
                 "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
-                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
                         "PREFIX sante: <http://www.semanticweb.org/msi/ontologies/2023/9/sante_ont#>\n" +
                         "SELECT ?nom ?specialite ?age ?experience ?adresse ?patientName\n" +
                         "WHERE {\n" +
-                        "  ?professionnel_de_la_sante rdf:type sante:Professionnel_de_la_sante ;\n" +
-                        "                            sante:aPourNom ?nom ;\n" +
-                        "                            sante:aPourSpecialite ?specialite ;\n" +
-                        "                            sante:aPourAge ?age ;\n" +
-                        "                            sante:aPourExperience ?experience ;\n" +
-                        "                            sante:apourAdresse ?adresse ;\n" +
-                       "                            sante:aConsulté ?patient .\n" +
-                        "\n" +
-                        "  ?patient sante:aPourNom ?patientName .\n" +
+                        "   ?prof rdf:type/rdfs:subClassOf* sante:Professionnel_de_la_sante .\n" +
+                       "  ?prof sante:aPourNom ?nom.\n" +
+                        "  ?prof sante:aPourSpecialite ?specialite.\n" +
+                        "  ?prof sante:aPourAge ?age.\n" +
+                        "  ?prof sante:aPourExperience ?experience.\n" +
+                        "  ?prof sante:apourAdresse ?adresse.\n" +
+                      "  ?professionnel_de_la_sante sante:aConsulté ?patient.\n" +
+                        "  ?patient sante:aPourNom ?patientName.\n" +
                         "}";
 
         return ResponseEntity.ok(rdfService.queryRDFJson(query));
---
+++ intelij-exec/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+@
+Famille_test.iml,a\d\ad4de1febf21bae33e206795ca7f50912220390e
+L
+data/queryProfessionnels.txt,8\5\856b8c7ef68dea1989a419e5f4149e74181cc0a1
+C
+data/sante_onto.owl,6\7\670fecb82e8e46c5f9a327f8f8f6692fb698c8e8
+<
+data/web.rdf,f\0\f0a8769d12fb4ee833a24131ed9328e9b1acb358
+F
+data/WebSemantique.rdf,b\c\bc1f79840d4867332e61b3f46779d5030b861d03
+P
+ src/esprit/application/Main.java,0\9\0977133ff7a3a6cca086e1c103c61cf097950f80
+>
+data/rules.txt,b\e\beff19e1eae8e2fe4b4b2936dc08afc8dac7ca41
