635829bdaffa3260cc3c41c786ad0f23b3d113ff
Manaa Ahmed <ahmed.manaa@esprit.tn>
2023-10-30 14:54:00 +0100

    ProductOntFix

--- antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/ProductController.java
+++ antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/ProductController.java
@@ -5,9 +5,11 @@ import io.mkadmi.antologieapi.services.RDFService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600,allowCredentials = "true")
 @Controller
 @RequestMapping("/product")
 public class ProductController {
--- frontend/app/products/productsComponent.js
+++ frontend/app/products/productsComponent.js
@@ -1,27 +1,58 @@
 "use client"
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 
-const ProductComponent = ({ product }) => {
+const ProductComponent = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8020/product/list")
+            .then(response => {
+                setProducts(response.data); // Assuming the response contains an array of products
+            })
+            .catch(error => {
+                console.error("Error fetching products:", error);
+            });
+    }, []);
+
+    const cardStyle = {
+        display: "flex",
+        flex: "0 0 30%",
+        margin: "1%",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center"
+    };
+
+    const productInfoStyle = {
+        textAlign: "center",
+    };
 
     return (
-        <Card className="py-4">
-            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                <p className="text-tiny uppercase font-bold">name</p>
-                <small className="text-default-500">Dosage: dosage</small>
-                <small className="text-default-500">Side Effects: sideEffects</small>
-                <small className="text-default-500">Instructions: instructions</small>
-                <small className="text-default-500">Pharmacy: pharmacy</small>
-            </CardHeader>
-            <CardBody className="overflow-visible py-2">
-                <Image
-                    alt="Product Image"
-                    className="object-cover rounded-xl"
-                    src="/images/image.jpg" 
-                    width={270}
-                />
-            </CardBody>
-        </Card>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+            {products.map(product => (
+                <Card className="py-4" key={product.id} style={cardStyle}>
+                    <CardHeader className="pb-0 pt-2 px-4" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <div style={productInfoStyle}>
+                            <p className="text-tiny uppercase font-bold">{product.aPourNom}</p>
+                            <small className="text-default-500">Dosage: {product.aAvecDosage}</small><br></br>
+                            <small className="text-default-500">Side Effects: {product.aDesEffetsSecondaires}</small><br></br>
+                            <small className="text-default-500">Instructions: {product.aDesInstructions}</small><br></br>
+                            <small className="text-default-500">Pharmacy: {product.pharmacieName}</small><br></br>
+                        </div>
+                    </CardHeader>
+                    <CardBody className="overflow-visible py-2" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <Image
+                            alt="Product Image"
+                            className="object-cover rounded-xl"
+                            src={"/images/medicaments.png"}
+                            width={270}
+                        />
+                    </CardBody>
+                </Card>
+            ))}
+        </div>
     );
 };
 
