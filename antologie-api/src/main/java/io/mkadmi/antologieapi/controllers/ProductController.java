ebb18cc674fd4e15cc6ed8ce214fda10d8e4db05
Manaa Ahmed <ahmed.manaa@esprit.tn>
2023-10-30 15:34:13 +0100

    ProductFrontDone

--- antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/ProductController.java
+++ antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/ProductController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600,allowCredentials = "true")
 @Controller
 @RequestMapping("/product")
 @CrossOrigin("*")
--- frontend/app/products/productsComponent.js
+++ frontend/app/products/productsComponent.js
@@ -1,10 +1,13 @@
 "use client"
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, Image, Button , Input } from "@nextui-org/react";
+import Swal from "sweetalert2";
 
 const ProductComponent = () => {
     const [products, setProducts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
 
     useEffect(() => {
         axios.get("http://localhost:8020/product/list")
@@ -18,7 +21,7 @@ const ProductComponent = () => {
 
     const cardStyle = {
         display: "flex",
-        flex: "0 0 30%",
+        flex: "1 1 20%",
         margin: "1%",
         flexDirection: "column",
         justifyContent: "center",
@@ -29,17 +32,55 @@ const ProductComponent = () => {
         textAlign: "center",
     };
 
+    const showDetails = (product) => {
+        const details = `
+        Name: ${product.aPourNom}<br>
+        Dosage: ${product.aAvecDosage}<br>
+        Side Effects: ${product.aDesEffetsSecondaires}<br>
+        Instructions: ${product.aDesInstructions}<br>
+        Pharmacy: ${product.pharmacieName}<br>
+    `;
+
+        Swal.fire({
+            title: `${product.aPourNom}`,
+            html: `<div dangerouslySetInnerHTML={{ __html: ${details} </div>`,
+            customClass: {
+                popup: 'custom-swal-popup',
+            },
+            didOpen: () => {
+                const swalPopup = document.querySelector('.swal2-popup');
+                swalPopup.style.backgroundColor = 'black';
+                swalPopup.style.color = 'white';
+            }
+        });
+    };
+
+    const filteredProducts = products.filter(
+        (product) =>
+            product.aPourNom.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            product.pharmacieName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
+        <div>
+        <div className="flex justify-center items-center">
+        <Input
+          type="text"
+          label="Search by Name or Phamarcy Name"
+          className="max-w-xs"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <br></br>
         <div style={{ display: "flex", flexWrap: "wrap" }}>
-            {products.map(product => (
+            {filteredProducts.map(product => (
                 <Card className="py-4" key={product.id} style={cardStyle}>
                     <CardHeader className="pb-0 pt-2 px-4" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <div style={productInfoStyle}>
-                            <p className="text-tiny uppercase font-bold">{product.aPourNom}</p>
-                            <small className="text-default-500">Dosage: {product.aAvecDosage}</small><br></br>
-                            <small className="text-default-500">Side Effects: {product.aDesEffetsSecondaires}</small><br></br>
-                            <small className="text-default-500">Instructions: {product.aDesInstructions}</small><br></br>
-                            <small className="text-default-500">Pharmacy: {product.pharmacieName}</small><br></br>
+                            <p className="text-tiny uppercase font-bold">{product.aPourNom}</p><br></br>
+                            <small className="text-default-500">{product.aAvecDosage}</small><br></br>
+                            <small className="text-default-500">{product.aDesInstructions}</small><br></br>
                         </div>
                     </CardHeader>
                     <CardBody className="overflow-visible py-2" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
@@ -49,9 +90,15 @@ const ProductComponent = () => {
                             src={"/images/medicaments.png"}
                             width={270}
                         />
+                        <div className="flex flex-wrap gap-4 items-center">
+                            <Button color="default" onClick={() => showDetails(product)}>
+                                Show Details
+                            </Button>
+                        </div>
                     </CardBody>
                 </Card>
             ))}
+        </div>
         </div>
     );
 };
