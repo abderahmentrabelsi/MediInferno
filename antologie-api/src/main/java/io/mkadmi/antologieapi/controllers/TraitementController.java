85ef2872335853d32322c28945fb9d339f283e59
khalil <khalil.turki@esprit.tn>
2023-10-30 23:51:15 +0100

    front end fixed

--- antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/TraitementController.java
+++ antologie-api/src/main/java/io/mkadmi/antologieapi/controllers/TraitementController.java
@@ -57,4 +57,31 @@ public class TraitementController {
         return ResponseEntity.ok(rdfService.queryRDFJson(query));
     }
 
+
+    @GetMapping("/list/{traitName}")
+    public ResponseEntity<JsonNode> findTraitementByTraitName(@PathVariable String traitName) {
+        try {
+            // SPARQL query to find a specific treatment by aPourNomTrait
+            String query = "PREFIX sante: <http://www.semanticweb.org/msi/ontologies/2023/9/sante_ont#>\n" +
+                    "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                    "SELECT ?aPourNomTrait ?aPourMateriels ?aPourDureeTrait ?aPourMedicaments ?aPourUnProfDeSante\n" +
+                    "WHERE {\n" +
+                    "  ?traitement rdf:type/rdfs:subClassOf* sante:Traitement.\n" +
+                    "  ?traitement sante:aPourNomTrait ?aPourNomTrait.\n" +
+                    "  ?traitement sante:aPourMateriels ?aPourMateriels.\n" +
+                    "  ?traitement sante:aPourDureeTrait ?aPourDureeTrait.\n" +
+                    "  ?traitement sante:aPourMedicaments ?aPourMedicaments.\n" +
+                    "  ?traitement sante:aPourUnProfDeSante ?aPourUnProfDeSante.\n" +
+                    "  FILTER(STR(?aPourNomTrait) = \"" + traitName + "\")\n" +
+                    "}";
+
+            return ResponseEntity.ok(rdfService.queryRDFJson(query));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build(); // Return a 500 Internal Server Error response
+        }
+    }
+
+
+
 }
--- frontend/app/traitements/productsComponent.js
+++ frontend/app/traitements/productsComponent.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Card, CardHeader, CardBody, Image, Button, Input } from "@nextui-org/react";
@@ -6,22 +6,60 @@ import Swal from "sweetalert2";
 
 const TraitementComponent = () => {
     const [traitements, setTraitements] = useState([]);
+    const [traitementNames, setTraitementNames] = useState([]);
+    const [professorNames, setProfessorNames] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [selectedTraitement, setSelectedTraitement] = useState("");
+    const [selectedProfessor, setSelectedProfessor] = useState("");
+    const [forceRender, setForceRender] = useState(false);
 
     useEffect(() => {
         axios
             .get("http://localhost:8020/traitements/list")
             .then((response) => {
-                console.log(response.data);
-                setTraitements(response.data); // Assuming the response contains an array of traitements
+                console.log("Fetched data:", response.data);
+                setTraitements(response.data);
+
+                const uniqueNames = [...new Set(response.data.map((traitement) => traitement.aPourNomTrait))];
+                console.log("Traitement Names:", uniqueNames);
+                setTraitementNames(uniqueNames);
+
+                const uniqueProfessors = [...new Set(response.data.map((traitement) => traitement.aPourUnProfDeSante))];
+                console.log("Professor Names:", uniqueProfessors);
+                setProfessorNames(uniqueProfessors);
             })
             .catch((error) => {
                 console.error("Error fetching traitements:", error);
             });
-    }, []);
+    }, [forceRender]);
+
+    useEffect(() => {
+        if (selectedTraitement) {
+            axios
+                .get(`http://localhost:8020/traitements/findByTraitName/${selectedTraitement}`)
+                .then((response) => {
+                    console.log(`Fetched data for ${selectedTraitement}:`, response.data);
+                    // Handle the response data as needed
+                })
+                .catch((error) => {
+                    console.error(`Error fetching data for ${selectedTraitement}:`, error);
+                });
+        }
+
+        if (selectedProfessor) {
+            axios
+                .get(`http://localhost:8020/traitements/findByProfessor/${selectedProfessor}`)
+                .then((response) => {
+                    console.log(`Fetched data for ${selectedProfessor}:`, response.data);
+                    // Handle the response data as needed
+                })
+                .catch((error) => {
+                    console.error(`Error fetching data for ${selectedProfessor}:`, error);
+                });
+        }
+    }, [selectedTraitement, selectedProfessor]);
 
     const cardStyle = {
-        display: "flex",
         flex: "1 1 20%",
         margin: "1%",
         flexDirection: "column",
@@ -62,6 +100,27 @@ const TraitementComponent = () => {
             traitement.aPourUnProfDeSante.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
+    // Log state changes and render cycles
+    useEffect(() => {
+        console.log("Traitement names changed:", traitementNames);
+    }, [traitementNames]);
+
+    useEffect(() => {
+        console.log("Selected traitement changed:", selectedTraitement);
+        // Trigger a re-render
+        setForceRender((prev) => !prev);
+    }, [selectedTraitement]);
+
+    useEffect(() => {
+        console.log("Selected professor changed:", selectedProfessor);
+        // Trigger a re-render
+        setForceRender((prev) => !prev);
+    }, [selectedProfessor]);
+
+    useEffect(() => {
+        console.log("Filtered traitements changed:", filteredTraitements);
+    }, [filteredTraitements]);
+
     return (
         <div>
             <div className="flex justify-center items-center">
@@ -73,8 +132,44 @@ const TraitementComponent = () => {
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </div>
-            <br></br>
-            <div style={{ display: "flex", flexWrap: "wrap" }}>
+            <br />
+            <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+                {traitementNames.length > 0 ? (
+                    <select
+                        value={selectedTraitement}
+                        onChange={(e) => setSelectedTraitement(e.target.value)}
+                        readOnly
+                    >
+                        <option value="">All Treatments</option>
+                        {traitementNames.map((name) => (
+                            <option key={name} value={name}>
+                                {name}
+                            </option>
+                        ))}
+                    </select>
+                ) : (
+                    <p>No traitement names available.</p>
+                )}
+
+                {professorNames.length > 0 ? (
+                    <select
+                        value={selectedProfessor}
+                        onChange={(e) => setSelectedProfessor(e.target.value)}
+                        readOnly
+                    >
+                        <option value="">All Professors</option>
+                        {professorNames.map((professor) => (
+                            <option key={professor} value={professor}>
+                                {professor}
+                            </option>
+                        ))}
+                    </select>
+                ) : (
+                    <p>No professor names available.</p>
+                )}
+            </div>
+            <br />
+            <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
                 {filteredTraitements.map((traitement) => (
                     <Card className="py-4" key={traitement.id} style={cardStyle}>
                         <CardHeader
@@ -83,18 +178,23 @@ const TraitementComponent = () => {
                         >
                             <div style={traitementInfoStyle}>
                                 <p className="text-tiny uppercase font-bold">{traitement.aPourNomTrait}</p>
-                                <br></br>
+                                <br />
                                 <small className="text-default-500">{traitement.aPourDureeTrait}</small>
-                                <br></br>
+                                <br />
                                 <small className="text-default-500">{traitement.aPourMedicaments}</small>
-                                <br></br>
+                                <br />
                             </div>
                         </CardHeader>
                         <CardBody
                             className="overflow-visible py-2"
                             style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
                         >
-                            <Image alt="Product Image" className="object-cover rounded-xl" src={"/images/Traitement.PNG"} width={270} />
+                            <Image
+                                alt="Product Image"
+                                className="object-cover rounded-xl"
+                                src={"/images/Traitement.PNG"}
+                                width={270}
+                            />
                             <div className="flex flex-wrap gap-4 items-center">
                                 <Button color="default" onClick={() => showDetails(traitement)}>
                                     Show Details
